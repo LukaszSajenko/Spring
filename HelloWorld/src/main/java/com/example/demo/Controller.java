27f9082ee8b7c997464a8dfccb30ca3d53f6d6b5
Łukasz Sajenko <lsajenko121@gmail.com>
2019-03-14 21:27:41 +0100

    Add files via upload

--- HelloWorld/src/main/java/com/example/demo/Controller.java
+++ HelloWorld/src/main/java/com/example/demo/Controller.java
@@ -3,11 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.text.DateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 @RestController
 @RequestMapping("/api")
@@ -15,7 +11,6 @@ public class Controller {
 
 	@GetMapping("/hello")
 	public String HelloWorld() {
-
 		return "Hello World!";
 	}
 	@GetMapping("/date")
